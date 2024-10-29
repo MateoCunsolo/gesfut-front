573c0130985cef843e4e2c55e14f71be7cf71195
Mateo Cunsolo <cunsolo73@gmail.com>
2024-10-29 13:54:24 -0300

    init: reorganizacion de la estructura

--- gesfut-front/src/app/app.routes.ts
+++ gesfut-front/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
+import { LandingComponent } from './guest/landing/landing.component';
 
 export const routes: Routes = [
+    //path: '', loadChildren : () => import('./admin/admin.routes').then(m =>m.admin_ROUTES)
+
+    
     {
         path: '', component: LandingComponent
     }
