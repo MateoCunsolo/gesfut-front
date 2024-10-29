9d73be017275b3fb09aaab39295fbd155e8be66f
Rafael Toresani <rtoresani@gmail.com>
2024-10-29 16:44:57 -0300

    add: formulario de registro

--- gesfut-front/src/app/auth/auth.routes.ts
+++ gesfut-front/src/app/auth/auth.routes.ts
@@ -1,10 +1,14 @@
 
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 export const AUTH_ROUTES: Routes = [
 
     {
         path: 'login', component: LoginComponent
+    },
+    {
+        path: 'singup', component: RegisterComponent
     }
 ];
 
--- gesfut-front/src/app/auth/register/register.component.ts
+++ gesfut-front/src/app/auth/register/register.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../../core/services/auth.service';
+import { ChangeDetectorRef } from '@angular/core';
+import { RegisterRequest } from '../../core/models/registerRequest';
+
+
+const materialModules = [
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatButtonModule
+];
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [materialModules, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+
+  registerForm:FormGroup;
+
+
+  constructor(private fb:FormBuilder, private authService:AuthService, private cdRef:ChangeDetectorRef){
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit() {
+    const credentials: RegisterRequest = this.registerForm.value; 
+    console.log('Datos enviados al servidor:', credentials)
+    this.authService.register(credentials).subscribe({
+      next: (response) => {
+        alert(response.firstName + ' ' + response.lastName);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+  
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
 
 }
