b6a572fcbcd15227c971ec5ac80fd6bd5990d9e9
Mateo Cunsolo <cunsolo73@gmail.com>
2024-10-29 15:23:43 -0300

    add: login realizado

---
+++ gesfut-front/src/app/auth/login/login.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { AuthService } from '../../core/services/auth.service';
+import { LoginRequest } from '../../core/models/loginRequest';
+const materialModules = [
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatButtonModule
+];
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, ...materialModules], // Agrega ReactiveFormsModule aquí
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],  // Cambié `styleUrl` a `styleUrls`
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      let loginRequest: LoginRequest = this.loginForm.value;
+        this.authService.login(loginRequest).subscribe({
+        next: (response) => {
+          console.log('Login successful', response);
+          //redirect to home client - dashboard
+        },
+        error: (err) => {
+          console.error('Login failed', err);
+        },
+        complete: () => {
+          console.log('Login request completed');
+        },
+      });
+    }
+  }
+
+}  
---
+++ gesfut-front/src/app/core/models/authResponse.ts
@@ -0,0 +1,11 @@
+
+export interface AuthResponse {
+
+    firstName: string;
+    lastName: string;
+    token: string;
+    role: string;
+
+    
+
+}
