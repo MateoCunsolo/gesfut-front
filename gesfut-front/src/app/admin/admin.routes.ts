b6ae8dc6ae271b0bcd11f205244cb93034f3a26b
Rafael Toresani <rtoresani@gmail.com>
2024-10-31 16:16:39 -0300

    add: crear equipo

--- gesfut-front/src/app/admin/admin.routes.ts
+++ gesfut-front/src/app/admin/admin.routes.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateTeamComponent } from './create-team/create-team.component';
 
 export const ADMIN_ROUTES: Routes = [
     {
         path: '', component:DashboardComponent
     },
+    {
+        path: 'crear-equipo', component:CreateTeamComponent 
+    }
+    ,
     {
         path: '**', redirectTo:''
     }
