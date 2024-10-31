edfd2c8393b91f6a2bea067f82db0911dea842e3
Mateo Cunsolo <cunsolo73@gmail.com>
2024-10-31 10:36:27 -0300

    add: fix creacion del torneo

--- gesfut-front/src/app/admin/dashboard/dashboard.component.ts
+++ gesfut-front/src/app/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';  // Importa Router
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss'],  // Debe ser styleUrls en plural
 })
 export class DashboardComponent {
+  constructor(private router: Router) {}  // Inyecta Router aquí
 
+  goToTournaments() {
+    this.router.navigate(['/admin/tournaments']);  // Usa this.router para navegar
+  }
 }
