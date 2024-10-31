af5996cd9aaa1aa0b99c6130b4d007f2102749ef
Mateo Cunsolo <cunsolo73@gmail.com>
2024-10-31 10:28:49 -0300

    add: creacion del torneo

--- gesfut-front/src/app/admin/admin.routes.ts
+++ gesfut-front/src/app/admin/admin.routes.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateTournamentComponent } from './create-tournament/create-tournament.component';
+import { TournamentDashboardComponent } from './tournament-dashboard/tournament-dashboard.component';
 
 export const ADMIN_ROUTES: Routes = [
     {
         path: '', component:DashboardComponent
     },
+    {
+        path: 'tournaments', component: CreateTournamentComponent
+    },
+    {
+        path: 'tournaments/:code', component: TournamentDashboardComponent
+    }
+    ,
     {
         path: '**', redirectTo:''
     }
