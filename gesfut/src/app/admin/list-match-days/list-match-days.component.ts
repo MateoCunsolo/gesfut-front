c7fab4a020f54241a62ab13ebdbd137384a0191d
Rafael Toresani <rtoresani@gmail.com>
2024-11-05 14:01:09 -0300

    update: list match days

--- gesfut/src/app/admin/list-match-days/list-match-days.component.ts
+++ gesfut/src/app/admin/list-match-days/list-match-days.component.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AdminService } from '../../core/services/admin.service';
-import { TournamentResponseFull } from '../../core/models/tournamentResponse';
-import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
+import { TournamentResponseFull } from '../../core/models/tournamentResponse';
 
 @Component({
   selector: 'app-list-match-days',
@@ -13,22 +11,33 @@ import { Subscription } from 'rxjs';
   templateUrl: './list-match-days.component.html',
   styleUrls: ['./list-match-days.component.scss']
 })
-export class ListMatchDaysComponent implements OnInit {
+export class ListMatchDaysComponent implements OnInit, OnDestroy {
   tournament: TournamentResponseFull | null = null;
   private subscription: Subscription | null = null;
-  selectedMatchDay=0;
+  selectedMatchDay = 0;
 
   constructor(private adminService: AdminService) {}
 
   ngOnInit(): void {
-    this.adminService.getTournament("246e0883-99ad-4f7e-a47f-a1c876e288e5").subscribe({
+    this.subscription = this.adminService.getTournament("be3a2687-5291-4617-bb1a-ed03406bc2fd").subscribe({
       next: (response) => {
         this.tournament = response;
+        this.sortMatchDays(); // Llama a la función para ordenar matchDays
       }
-    })
+    });
   }
 
   ngOnDestroy(): void {
     this.subscription?.unsubscribe();
+  }
+
+  updateMatchDay(matchDay: number) {
+    this.selectedMatchDay = matchDay;
+  }
+
+  private sortMatchDays(): void {
+    if (this.tournament?.matchDays) {
+      this.tournament.matchDays.sort((a, b) => a.numberOfMatchDay - b.numberOfMatchDay);
+    }
   }
 }
