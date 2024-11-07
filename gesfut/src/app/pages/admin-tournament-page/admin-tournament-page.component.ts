09375834f08dead805fb7fff9dbd0667d349a266
Rafael Toresani <rtoresani@gmail.com>
2024-11-07 20:27:26 -0300

    add: load result

--- gesfut/src/app/pages/admin-tournament-page/admin-tournament-page.component.ts
+++ gesfut/src/app/pages/admin-tournament-page/admin-tournament-page.component.ts
@@ -3,18 +3,19 @@ import { AfterViewInit, Component} from '@angular/core';
 import { NavbarComponent } from "../../admin/navbar/navbar.component";
 import { TournamentDashboardComponent } from "../../admin/tournament-dashboard/tournament-dashboard.component";
 import { InitializeTournamentComponent } from "../../admin/initialize-tournament/initialize-tournament.component";
-import { ListMatchDaysComponent } from "../../admin/list-match-days/list-match-days.component";
+import { ListMatchDaysComponent } from "../../admin/match-day/list-match-days/list-match-days.component";
 import { DashboardService } from '../../core/services/dashboard.service';
 import { combineLatest } from 'rxjs';
 import { TournamentService } from '../../core/services/tournament/tournament.service';
 import { ParticipantResponseShort } from '../../core/models/participantResponse';
 import { ActivatedRoute } from '@angular/router';
+import { MatchDaysComponent } from "../../admin/match-day/match-days/match-days.component";
 
 
 @Component({
   selector: 'app-admin-tournament-page',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, TournamentDashboardComponent, InitializeTournamentComponent, ListMatchDaysComponent],
+  imports: [CommonModule, NavbarComponent, TournamentDashboardComponent, InitializeTournamentComponent, ListMatchDaysComponent, MatchDaysComponent],
   templateUrl: './admin-tournament-page.component.html',
   styleUrl: './admin-tournament-page.component.scss'
 })
@@ -32,24 +33,17 @@ export class AdminTournamentPageComponent implements AfterViewInit {
   ){}
 
   ngOnInit() {
-
     this.dashboardService.haveParticipants$.subscribe((thereAre:boolean)=>{
       this.flag = thereAre;
     })
-
-
-    console.log('ngOnInit ejecutado');
     this.dashboardService.activeTournamentComponent$.subscribe((component:string)=>{
       this.activeComponent = component;
-      console.log('despues de actualizar: ' + this.activeComponent);
     })
   }
 
   ngAfterViewInit() {
-    console.log('ngOnInit ejecutado');
     this.dashboardService.activeTournamentComponent$.subscribe((component:string)=>{
       this.activeComponent = component;
-      console.log('despues de actualizar: ' + this.activeComponent);
     })
   }
 }
