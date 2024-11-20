1f87fab4688eac20289c3640830bf6f0c7b2782e
Mateo Cunsolo <cunsolo73@gmail.com>
2024-11-20 16:58:04 -0300

    add: sweets alerts

--- gesfut/src/app/guest/landing/landing.component.ts
+++ gesfut/src/app/guest/landing/landing.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
-import { ListTeamsComponent } from "../../admin/list-teams/list-teams.component";
+import { TestSweetComponent } from "../../test-sweet/test-sweet.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterModule, ListTeamsComponent],
+  imports: [RouterModule, TestSweetComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
