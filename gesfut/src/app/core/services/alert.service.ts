7659a281cc9ac817c5c8ca1b358fdc866633fb20
Mateo Cunsolo <cunsolo73@gmail.com>
2024-11-25 14:09:13 -0300

    add: sweet alerts, loading y advertencia

--- gesfut/src/app/core/services/alert.service.ts
+++ gesfut/src/app/core/services/alert.service.ts
@@ -8,29 +8,52 @@ export class AlertService {
 
   constructor() { }
 
-  successAlert(title:string){
+  successAlert(title: string) {
     Swal.fire({
       icon: "success",
       title: title,
       showConfirmButton: false,
-      timer: 1500
+      timer: 1600
     });
   }
 
   errorAlert(err: string) {
     Swal.fire({
-      toast: true, 
-      icon: 'error', 
-      title: err, 
-      position: 'top-end', 
-      showConfirmButton: false, 
-      timer: 3000, 
-      timerProgressBar: true, 
+      toast: true,
+      icon: 'error',
+      title: err,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
       customClass: {
-        container: 'container-toast', 
-        icon: 'toast-icon', 
+        container: 'container-toast',
+        icon: 'toast-icon',
         title: 'toast-title'
       },
     });
   }
+
+  loadingAlert(title: string) {
+    Swal.fire({
+      title: title,
+      didOpen: () => {
+        Swal.showLoading();
+      }
+    });
+  }
+
+  confirmAlert(title: string, text: string, confirmButtonText: string) {
+    return Swal.fire({
+      title: title,
+      text: text,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: confirmButtonText
+    });
+  }
+
+
 }
