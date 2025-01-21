8e8979941451d27b35791d2844e15bb489169e0d
Rafa <rtoresani@gmail.com>
2025-01-21 16:46:27 -0300

    add: alerts on prize components

--- gesfut/src/app/admin/prizes/create-prize/create-prize.component.ts
+++ gesfut/src/app/admin/prizes/create-prize/create-prize.component.ts
@@ -1,28 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { PrizeService } from '../../../core/services/tournament/prize.service';
 import { PrizesRequest } from '../../../core/models/prizesRequest';
+import { AlertService } from '../../../core/services/alert.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-prize',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-prize.component.html',
-  styleUrl: './create-prize.component.scss'
+  styleUrl: './create-prize.component.scss',
 })
 export class CreatePrizeComponent {
-
   prizesForm: FormGroup;
-  category:string = '';
+  category: string = '';
 
-  constructor(private fb: FormBuilder, private prizeService:PrizeService) {
+  constructor(
+    private fb: FormBuilder,
+    private prizeService: PrizeService,
+    private alertService: AlertService
+  ) {
     this.prizesForm = this.fb.group({
       prizes: this.fb.array([]),
-      code: ['']
+      code: [''],
     });
 
-    const storedCode = localStorage.getItem("lastTournamentClicked");
+    const storedCode = localStorage.getItem('lastTournamentClicked');
     if (storedCode) {
       this.prizesForm.patchValue({ code: storedCode });
     }
@@ -30,8 +41,8 @@ export class CreatePrizeComponent {
     this.prizeService.currentCategory.subscribe({
       next: (category) => {
         this.category = category;
-      }
-    })
+      },
+    });
   }
 
   get prizes(): FormArray {
@@ -41,14 +52,13 @@ export class CreatePrizeComponent {
   addPrize() {
     if (this.category !== '') {
       const prizeGroup = this.fb.group({
-        type:[this.category, Validators.required],
+        type: [this.category, Validators.required],
         description: ['', Validators.required],
-        position: [0, [Validators.required, Validators.min(1)]]
+        position: [0, [Validators.required, Validators.min(1)]],
       });
       this.prizes.push(prizeGroup);
     }
   }
-
 
   removePrize() {
     if (this.prizes.length > 0) {
@@ -63,18 +73,19 @@ export class CreatePrizeComponent {
         prizes: formValue.prizes.map((prize: any) => ({
           type: prize.type,
           description: prize.description,
-          position: prize.position
+          position: prize.position,
         })),
-        code: formValue.code
+        code: formValue.code,
       };
 
       this.prizeService.sendPrizes(prizesRequest).subscribe({
         next: () => {
-          console.log('Premios enviados correctamente');
+          this.alertService.successAlert('Premios agregados!');
         },
-        error: (error) => {
-          console.error('Error al enviar los premios:', error);
-        }
+        error: (err: HttpErrorResponse) => {
+          console.log(err);
+          this.alertService.errorAlert(err.error.error);
+        },
       });
     }
   }
--- gesfut/src/app/admin/prizes/list-prizes/list-prizes.component.ts
+++ gesfut/src/app/admin/prizes/list-prizes/list-prizes.component.ts
@@ -3,30 +3,36 @@ import { PrizeService } from '../../../core/services/tournament/prize.service';
 import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { Prize } from '../../../core/models/prizesRequest';
+import { AlertService } from '../../../core/services/alert.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-prizes',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './list-prizes.component.html',
-  styleUrls: ['./list-prizes.component.scss']
+  styleUrls: ['./list-prizes.component.scss'],
 })
 export class ListPrizesComponent implements OnInit, OnDestroy {
-
-  currentCategory: string = "";
-  prizes:Prize[] = [];
+  currentCategory: string = '';
+  prizes: Prize[] = [];
   categorySubscription: Subscription | undefined;
-  currentTournament: string | null = localStorage.getItem("lastTournamentClicked");
+  currentTournament: string | null = localStorage.getItem(
+    'lastTournamentClicked'
+  );
 
-
-  constructor(private prizeService: PrizeService) {}
+  constructor(
+    private prizeService: PrizeService,
+    private alertService: AlertService
+  ) {}
 
   ngOnInit() {
-    this.categorySubscription = this.prizeService.currentCategory
-      .subscribe((newCategory: string) => {
+    this.categorySubscription = this.prizeService.currentCategory.subscribe(
+      (newCategory: string) => {
         this.currentCategory = newCategory;
         this.getPrizes();
-      });
+      }
+    );
   }
 
   ngOnDestroy() {
@@ -35,14 +41,16 @@ export class ListPrizesComponent implements OnInit, OnDestroy {
     }
   }
 
-  getPrizes(){
-    if(this.currentTournament)
-    this.prizeService.getAllPrizesByCategory(this.currentTournament, this.currentCategory).subscribe({
-      next: (response:Prize[]) => {
-        console.log(this.currentCategory)
-        this.prizes = response;
-      }
-    })
+  getPrizes() {
+    if (this.currentTournament)
+      this.prizeService
+        .getAllPrizesByCategory(this.currentTournament, this.currentCategory)
+        .subscribe({
+          next: (response: Prize[]) => {
+            console.log(this.currentCategory);
+            this.prizes = response;
+          },
+        });
   }
 
   expandedIndex: number | null = null;
@@ -56,27 +64,32 @@ export class ListPrizesComponent implements OnInit, OnDestroy {
   }
 
   setView(view: string) {
-    console.log("Cambiando vista a:", view);
+    console.log('Cambiando vista a:', view);
     this.prizeService.currentView.next(view);
   }
 
-  deletePrize(position: number) {
-    if (this.currentTournament) {
-      const confirmDelete = window.confirm("¿Estás seguro de que deseas eliminar este premio?");
-
-      if (confirmDelete) {
-        this.prizeService.deletePrize(this.currentTournament, this.currentCategory, position).subscribe({
-          next: () => {
-            this.prizes = this.prizes.filter(prize => prize.position !== position);
-            alert("Premio eliminado con éxito.");
-          },
-          error: (error) => {
-            console.log(error);
-            alert("Ocurrió un error al eliminar el premio.");
-          }
-        });
-      }
-    }
+  async deletePrize(position: number) {
+    this.alertService
+      .confirmAlert(
+        '¿Desea eliminar el premio?',
+        'Confirma para eliminar.',
+        'Confirmar'
+      )
+      .then((result) => {
+        if (result.isConfirmed && this.currentTournament)
+          this.prizeService
+            .deletePrize(this.currentTournament, this.currentCategory, position)
+            .subscribe({
+              next: () => {
+                this.prizes = this.prizes.filter(
+                  (prize) => prize.position !== position
+                );
+              },
+              error: (err: HttpErrorResponse) => {
+                console.log(err);
+                this.alertService.errorAlert(err.error.error);
+              },
+            });
+      });
   }
-
 }
